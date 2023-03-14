9d4c78daa7cbd50f58e753777c9b9b2d1c42eda8
GET Academy <terje@getacademy.no>
2023-03-14 11:34:59 +0100

    Update MyConsole.cs

--- IntroM3/MyConsole.cs
+++ IntroM3/MyConsole.cs
@@ -2,13 +2,13 @@
 {
     internal class MyConsole
     {
-        static string Ask(string question)
+        public static string Ask(string question)
         {
             Console.Write(question);
             return Console.ReadLine();
         }
 
-        static int AskForInt(string question)
+        public static int AskForInt(string question)
         {
             var answer = Ask(question);
             return Convert.ToInt32(answer);
