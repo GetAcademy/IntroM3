bc2c476c6d50574badea0370e9c10f11c46c6c66
GET Academy <terje@getacademy.no>
2023-03-14 11:34:26 +0100

    Initial commit

---
+++ IntroM3/MyConsole.cs
@@ -0,0 +1,17 @@
+﻿namespace IntroM3
+{
+    internal class MyConsole
+    {
+        static string Ask(string question)
+        {
+            Console.Write(question);
+            return Console.ReadLine();
+        }
+
+        static int AskForInt(string question)
+        {
+            var answer = Ask(question);
+            return Convert.ToInt32(answer);
+        }
+    }
+}
---
+++ IntroM3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace IntroM3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = MyConsole.Ask("Hva heter du? ");
+            Console.WriteLine($"Hei, {name}!");
+
+            int year= MyConsole.AskForInt("I hvilket år er du født? ");
+            int age = 2023 - year;
+            Console.WriteLine($"Da er du {age} år gammel.");
+        }
+    }
+}
